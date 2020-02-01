910cc8b36e934faec47a89d02ed28fdd09b38984
DanWoolford <60345708+DanWoolford@users.noreply.github.com>
2020-02-01 01:21:13 +0000

    Git Init

---
+++ PokeTypes/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PokeTypes.Models;
+
+namespace PokeTypes.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
