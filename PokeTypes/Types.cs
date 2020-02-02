8d6866d0e1e7a76abc7456895261ae4a9cdb3941
DanWoolford <60345708+DanWoolford@users.noreply.github.com>
2020-02-02 02:45:08 +0000

    Methods Progress

---
+++ PokeTypes/Types.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace PokeTypes
+{
+    public class Types
+    {
+
+        public static double GetEffectiveness(string a, string d)
+        {
+            double i = 1;
+            a = a.ToLower();
+            d = d.ToLower();
+
+            if(a == "normal")
+            {
+                if (d == "ghost") i = 0;
+                if (d == "steel") i = .5;
+                if (d == "rock") i = .5;
+            }
+            else if (a == "fire")
+            {
+                if (d == "fire") i = .5;
+                if (d == "water") i = .5;
+                if (d == "rock") i = .5;
+                if (d == "dragon") i = .5;
+                if (d == "grass") i = 2;
+                if (d == "ice") i = 2;
+                if (d == "bug") i = 2;
+                if (d == "steel") i = 2;
+            }
+            else if (a == "water")
+            {
+                if (d == "water") i = .5;
+                if (d == "grass") i = .5;
+                if (d == "dragon") i = .5;
+                if (d == "fire") i = 2;
+                if (d == "ground") i = 2;
+                if (d == "rock") i = 2;
+            }
+            else if (a == "electric")
+            {
+                if (d == "xxxxxxxx") i /= 2;
+                if (d == "xxxxxxxx") i /= 2;
+                if (d == "xxxxxxxx") i *= 2;
+            }
+
+            return i;
+        }
+    }
+}
---
+++ PokeTypes/Views/Attacking.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PokeTypes.Views
+{
+    public partial class Attacking : ContentPage
+    {
+        public Attacking()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                string a = picker.Items[picker.SelectedIndex];
+                string d = picker2.Items[picker2.SelectedIndex];
+                btn.Text = Types.GetEffectiveness(a, d).ToString();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                btn.Text = "idiot";
+            }
+            
+        }
+
+    }
+}
